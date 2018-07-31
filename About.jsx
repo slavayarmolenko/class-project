3061b1635fd63d9968eb2ee51193abf98d379bb3
Slava <slava.yarmolenko@gmail.com>
2018-07-31 14:34:03 -0700

    attaching team service

--- About.jsx
+++ About.jsx
@@ -8,9 +8,7 @@ class About extends React.Component {
             <div class="aboutText">
                 Since the Trump Administration repealed the DACA program in September 2017, we have been concerned for our friends and classmates whose safety, academic opportunities, and DREAMs for a bright future in the country they grew up in have been compromised.  
                 We are a group of Carlmont High School students who have tasked ourselves with building a safety net and speaking up for those who are now too afraid to speak for themselves. Come join our ranks, and together we can make a difference. 
-                Jenna Teterin Luz Victoria Valle Remond Nisha Marino
-
- Adrian Fernandez
+                Jenna Teterin Luz Victoria Valle Remond Nisha Marino Adrian Fernandez
             </div>
             
          </div>
--- Team.jsx
+++ Team.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactTable from 'react-table';
+import 'whatwg-fetch'; 
 
 class Team extends React.Component {
+    constructor() {
+      super();
+      var self = this;
+      this.state = {
+         data:[]
+      };
+          fetch('/team')
+            .then(res => res.json())
+            .then(team => this.setState({ data: team.data }));
+        /*var xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange = function() {
+           console.log(xhttp.responseText.length);
+           console.log('Team members:');
+           console.log(JSON.parse(xhttp.responseText));
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var data = JSON.parse(xhttp.responseText);
+          self.setState({ data: data });
+        }
+      };
+      xhttp.open("GET", "http://localhost:3000/team", true);
+      xhttp.send();*/
+    }
     render() {
-        const data = [{name: "Slava", role:"UI programmer/God, just god"},{name:"Vika", role:"CEO"}];
+        
 const columns = [{
     Header: 'Name',
     accessor: 'name' // String-based value accessors!
@@ -15,7 +38,7 @@ const columns = [{
         return (
             <div>
                 <ReactTable
-                data={data}
+                data={this.state.data}
                 columns={columns}
                 pageSize="10"
                 />
