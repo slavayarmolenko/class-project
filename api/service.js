c94197be0e23b467b32893494448c459a512e996
Slava <slava.yarmolenko@gmail.com>
2018-08-10 23:19:14 -0700

    React-backend is removed. MySQL new table lawyers

--- api/service.js
+++ api/service.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var app = express();
-
 var teamService = require('./team');
 var lawyersService = require('./lawyers');
-
 var name;
 var description;
 var email;
 var mysql = require('mysql');
-
 var connection = mysql.createConnection({
     host: "classdb.c1fc1qmtlpg9.us-west-1.rds.amazonaws.com",
     user: "master",
@@ -21,40 +18,17 @@ connection.connect(function (err) {
         throw err;
     console.log('You are now connected...');
     console.log("adding person");
-            //connection.query('INSERT INTO people (id,name, age, address) VALUES (?, ?, ?, ?)', ['4', 'Slava', '17', 'California, USA'], function (err, result) {
-        if (err)
-            throw err;
-
-        connection.query('SELECT * FROM people', function (err, results) {
-            if (err)
-                throw err;
-            console.log(results[2].id)
-            console.log(results[2].name)
-            console.log(results[2].age)
-            console.log(results[2].address)
-            connection.query('DELETE INTO people WHERE id = 1', function (err, result) {
-                console.log(results[2].id)
-                console.log(results[2].name)
-                console.log(results[2].age)
-                console.log(results[2].address)
-            })
-        })
-    //})
-    connection.query('INSERT INTO people (id,name, age, address) VALUES (?, ?, ?, ?)', ['4', 'Slava', '17', 'California, USA'], function (err, result) {
-        name = "";
-        email = "";
-        description = "";
-    })
-});
-
+    //connection.query('INSERT INTO people (id,name, age, address) VALUES (?, ?, ?, ?)', ['4', 'Slava', '17', 'California, USA'], function (err, result) {
+    if (err)
+        throw err;
+})
+            //})
 
 teamService.create(app);
 lawyersService.create(app);
-
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log("Example app listening at http://%s:%s", host, port);
 });
 
