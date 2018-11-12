34e75f4b7c2dbab9b5256c49f6f6c5df54c63459
Slava <slava.yarmolenko@gmail.com>
2018-11-12 04:20:38 -0800

    finally it is workinggit pull! At 4.20AM...

--- api/lawyers.js
+++ api/lawyers.js
@@ -66,17 +66,19 @@ exports.create = function (app) {
         var addNewLawyerLine2 = ') VALUES (';
         var i = 0;
         var columnNames = ["uzvername", "password", "email", "description", "name", "shortname", "russian", "spanish", "english", "zip", "daca", "family", "deportationProtection", "address"];
-        for (var propname in req.body) {
+        /*for (var propname in req.body) {
             var propvalue = req.body[propname];
-            if ((propname == "uzvername")||(propname == "password")||(propname != "email")||(propname != "description")||(propname != "name")||(propname != "shortname")||(propname != "address")){
+            if ((propname == "uzvername")||(propname == "password")||(propname == "email")||(propname == "description")||(propname == "name")||(propname == "shortname")||(propname == "address")){
                 if(propvalue != ""){
                     addNewLawyerLine1 = addNewLawyerLine1 + columnNames[i];
                     addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body[propname] + '"';
-                    if (propname != "address") {
+                    if (columnNames[i] != "address") {
                         addNewLawyerLine1 = addNewLawyerLine1 + ", ";
                         addNewLawyerLine2 = addNewLawyerLine2 + ", ";
+                        console.log(columnNames[i]);
                     } else {
                         addNewLawyerLine2 = addNewLawyerLine2 + ");";
+                        
                     } 
                 }
             } else if ((propname == "russian")||(propname == "spanish")||(propname == "english")||(propname == "daca")||(propname == "family")||(propname == "deportationProtection")){
@@ -85,24 +87,83 @@ exports.create = function (app) {
                     addNewLawyerLine2 = addNewLawyerLine2 + req.body[propname] + ", ";
                 }
             }  else if (propname == "zip"){
-                if(propvalue != ""){
+                if(propvalue != "00000"){
                     addNewLawyerLine1 = addNewLawyerLine1 + columnNames[i] + ", ";
                     addNewLawyerLine2 = addNewLawyerLine2 + req.body[propname] + ", ";
                 }
             }
             i++;
+        }*/
+        if(req.body.uzvername){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'uzvername, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.uzvername + '",';
         }
+        if(req.body.password){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'password, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.password + '",';
+        }
+        if(req.body.email){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'email, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.email + '",';
+        } 
+        if(req.body.description){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'description, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.description + '",';
+        }
+        if(req.body.name){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'name, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.name + '",';
+        }       
+        if(req.body.shortname){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'shortname, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.shortname + '",';
+        }
+        if(req.body.russian){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'russian, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.russian + ',';
+        }
+        if(req.body.spanish){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'spanish, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.spanish + ',';
+        }
+        if(req.body.english){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'english, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.english + ',';
+        }
+        if(req.body.zip){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'zip, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.zip + ',';
+        }
+        if(req.body.daca){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'daca, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.daca + ',';
+        }
+        if(req.body.family){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'family, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.family + ',';
+        }
+        if(req.body.deportationProtection){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'deportationProtection, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + req.body.deportationProtection + ',';
+        }
+        if(req.body.address){
+            addNewLawyerLine1 = addNewLawyerLine1 + 'address, ';
+            addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.address + '",';
+        }
+        addNewLawyerLine1 = addNewLawyerLine1.substring(0, addNewLawyerLine1.length - 2);
+        addNewLawyerLine2 = addNewLawyerLine2.substring(0, addNewLawyerLine2.length - 1) + ');';
         console.log("Here " + req.body);
 
 
-        console.log("addNewLawyerLine = " + addNewLawyerLine);
+        console.log(addNewLawyerLine1);
+        console.log(addNewLawyerLine2);
         connection.query(addNewLawyerLine1 + addNewLawyerLine2, function (err, results) {
             if (err) {
                 res.json({ success: false, errMessage: err.sqlMessage });
             } else {
                 res.json({ success: true, results: results });
             }
-            console.log(results);
+            
 
         });
     });
