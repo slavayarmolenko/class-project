bb80232ccf26b78531a620ed4e0920c73c7aa952
anna_yarmolenko <anna.yarmolenko@oracle.com>
2018-11-12 04:19:12 -0800

    Frontend for Add Lawyer for is quite ready

--- pages/AddLawyer.jsx
+++ pages/AddLawyer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
+import red from '@material-ui/core/colors/red';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -22,7 +23,9 @@ class AddLawyer extends React.Component {
                 spanish: false,
                 russian: false,
                 address: '',
-            }
+            },
+            errorText: '',
+            redirectToList: false
         };
         this.logged = true;
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -55,31 +58,43 @@ class AddLawyer extends React.Component {
     }
  
     handleSubmit() {
+        this.setState({ errorText: '' });
         axios.post('/api/lawyers', this.state.lawyer)
                 .then(result => {
-                    console.log('Submitted laywer');
-                    console.log(result.data);
-                    this.setState({
-                        data: result.data.data,
-                        dataLoaded: true
-                    });
-
+                    if (result.success) {
+                        this.goToList();
+                    } else {
+                        this.setState({
+                            errorText: error.response.statusText,
+                        });
+                    }
                 })
                 .catch(error => {
                     
                     this.setState({
-                        error: error.Error,
-                        success: false
+                        errorText: error.response.statusText,
                     });
-
                 });
+    }
+
+    goToList() {
+        this.setState({redirectToList: true});
     }
     
     render() {
         const { uzvername, name, email, password, repeatPassword, 
             description, zip, english, spanish, russian, address} = this.state.lawyer;
+        const errorText = this.state.errorText;
+        const red300 = red['500'];
+ 
+        const errStyle = {
+            color: red300,
+        };
 
         if (!this.logged) {
+            return <Redirect to='/login'  />;
+        }
+        if (this.state.redirectToList) {
             return <Redirect to='/lawyers'  />;
         }
         return (
@@ -89,6 +104,7 @@ class AddLawyer extends React.Component {
                 onSubmit={this.handleSubmit}
                 onError={errors => console.log(errors)}
             >   
+                <div style={errStyle}>{errorText}</div>
                 <div>
                 <TextValidator
                     label="Username"
@@ -165,7 +181,7 @@ class AddLawyer extends React.Component {
                     validators={[]}
                     errorMessages={[]}
                     value={address}
-                    fullWidth="true"
+                    fullWidth={true}
                 /></div>
                 <div>
                 <Checkbox 
@@ -173,7 +189,7 @@ class AddLawyer extends React.Component {
                         name="english"
                         value="1"
                         checked={english}
-                /><InputLabel for="english">English</InputLabel>
+                /><InputLabel htmlFor="english">English</InputLabel>
                 
                     <Checkbox 
                         label="Speak Spanish"
@@ -191,7 +207,7 @@ class AddLawyer extends React.Component {
                         value="1"
                 /><InputLabel>Russian</InputLabel></div>
 
-                <Button type="submit">Submit</Button>
+                <Button type="submit" color="primary" variant="contained">Submit</Button>
             </ValidatorForm>
             </div>
                    );
