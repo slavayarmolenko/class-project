679348596ea9dec4d0e30c84965d788e8c750c00
anna_yarmolenko <anna.yarmolenko@oracle.com>
2018-11-11 22:14:29 -0800

    Missed files are added

---
+++ pages/AddLawyer.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Redirect } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import axios from 'axios';
+
+
+class AddLawyer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+                name: '',
+                email: '',
+                int: 2
+        };
+        this.logged = true;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        var value = event.target.value;
+        const name = event.target.name;
+        console.log('Set ' + name + ' with value ' + value);
+        this.setState({
+            [name]: value
+          });
+    }
+ 
+    handleSubmit() {
+        axios.post('/api/lawyers', this.state)
+                .then(result => {
+                    console.log('Submitted laywer');
+                    console.log(result.data);
+                    this.setState({
+                        data: result.data.data,
+                        dataLoaded: true
+                    });
+
+                })
+                .catch(error => {
+                    console.log('error::');
+                    console.log('bebebe:' + error);
+                    this.setState({
+                        error: error.Error,
+                        dataLoaded: false
+                    });
+
+                });
+    }
+    
+    render() {
+        const { name, email } = this.state;
+        if (!this.logged) {
+            return <Redirect to='/lawyers'  />;
+        }
+        return (
+            <div class="container">
+            <ValidatorForm 
+                onSubmit={this.handleSubmit}
+                onError={errors => console.log(errors)}
+            >   
+                <div>
+                <TextValidator
+                    label="Name"
+                    onChange={this.handleChange}
+                    name="name"
+                    type="text"
+                    validators={['required']}
+                    errorMessages={['this field is required']}
+                    value={name}
+                /></div>
+                <div><TextValidator
+                    label="E-Mail"
+                    onChange={this.handleChange}
+                    name="email"
+                    type="email"
+                    value={email}
+                    validators={['required', 'isEmail']}
+                    errorMessages={['this field is required', 'email is not valid']}
+                /></div>
+                <Button type="submit">Submit</Button>
+            </ValidatorForm>
+            </div>
+                   );
+    }
+}
+export default AddLawyer;
