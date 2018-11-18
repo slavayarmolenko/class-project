ac7a600f88e851ddd420d139b0dcf228d1052d26
Slava <slava.yarmolenko@gmail.com>
2018-11-18 03:54:57 -0800

    dome something... IDK what and why...

--- api/lawyers.js
+++ api/lawyers.js
@@ -3,7 +3,7 @@ Vladislav Iarmolenko
 slava.yarmolenko@gmail.com
 Created: August 2018
 */
-
+var common = require('./common');
 var ZipCodes = require('zipcodes');
 
 exports.create = function (app, connection) {
@@ -12,7 +12,7 @@ exports.create = function (app, connection) {
         if (req.query.id) {
             getLawyerById(req, res);
             return;
-        }       
+        }
         connection.query('SELECT id, email, name FROM lawyers', function (err, results) {
             if (err)
                 throw err;
@@ -42,7 +42,7 @@ exports.create = function (app, connection) {
         //res.send('Hello');
     });
 
-    
+
 
     app.post('/api/lawyer', function (req, res) {
 
@@ -53,13 +53,59 @@ exports.create = function (app, connection) {
         var addNewLawyerLine2 = ') VALUES (';
         var i = 0;
         var ending = ");"
-        if(req.body.id){
+        if (req.body.id) {
             addNewLawyerLine1 = 'UPDATE lawyers SET ';
-            ending = " WHERE ID="+req.body.id+";";
+            ending = " WHERE id=" + req.body.id + ";";
             addNewLawyerLine2 = ' ';
         }
-        var column;
-        if(req.body.uzvername){
+        var isUpdate = req.body.id ? true : false;
+
+        var columnObject = [
+            {
+                type: "string",
+                id: "uzvername",
+                required: true
+            }, {
+                type: "string",
+                id: "password",
+                required: true
+            }, {
+                type: "string",
+                id: "email",
+                required: true
+            }, {
+                type: "string",
+                id: "description",
+                required: false
+            }, {
+                type: "string",
+                id: "name",
+                required: true
+            }, {
+                type: "string",
+                id: "shortname",
+                required: false
+            }, {
+                type: "number",
+                id: "zip",
+                required: true
+            }, {
+                type: "string",
+                id: "address",
+                required: false
+            }
+        ];
+        for (var i = 0; i < columnObject.length; i++) {
+            if (isUpdate) {
+                addNewLawyerLine1 += common.getUpdateValueString(columnObject[i], req.body[columnObject[i].id]);
+            } else {
+                addNewLawyerLine1 += common.getInsertNameString(columnObject[i], req.body[columnObject[i].id]);
+                addNewLawyerLine2 += common.getInsertValueString(columnObject[i], req.body[columnObject[i].id]);
+                console.log(req.body[columnObject[i].id]);
+            }
+            
+        }
+        /*if(req.body.uzvername){
             column = "uzvername";
             if(req.body.id){
                 addNewLawyerLine1 = addNewLawyerLine1 + column +'="'+req.body.uzvername+'", ';
@@ -122,33 +168,6 @@ exports.create = function (app, connection) {
             addNewLawyerLine2 = addNewLawyerLine2 + req.body.zip + ',';
             }
         }
-        if(req.body.daca){
-            column = "daca";
-            if(req.body.id){
-                addNewLawyerLine1 = addNewLawyerLine1 + column +'="'+req.body.daca+'", ';
-            } else {
-            addNewLawyerLine1 = addNewLawyerLine1 + column +', ';
-            addNewLawyerLine2 = addNewLawyerLine2 + req.body.daca + ',';
-            }
-        }
-        if(req.body.family){
-            column = "family";
-            if(req.body.id){
-                addNewLawyerLine1 = addNewLawyerLine1 + column +'="'+req.body.family+'", ';
-            } else {
-            addNewLawyerLine1 = addNewLawyerLine1 + column +', ';
-            addNewLawyerLine2 = addNewLawyerLine2 + req.body.family + ',';
-            }
-        }
-        if(req.body.deportationProtection){
-            column = "deportationProtection";
-            if(req.body.id){
-                addNewLawyerLine1 = addNewLawyerLine1 + column +'="'+req.body.deportationProtection+'", ';
-            } else {
-            addNewLawyerLine1 = addNewLawyerLine1 + column +', ';
-            addNewLawyerLine2 = addNewLawyerLine2 + req.body.deportationProtection + ',';
-            }
-        }
         if(req.body.address){
             column = "address";
             if(req.body.id){
@@ -157,9 +176,9 @@ exports.create = function (app, connection) {
             addNewLawyerLine1 = addNewLawyerLine1 + column +', ';
             addNewLawyerLine2 = addNewLawyerLine2 + '"' + req.body.address + '",';
             }
-        }
+        }*/
         addNewLawyerLine1 = addNewLawyerLine1.substring(0, addNewLawyerLine1.length - 2);
-        addNewLawyerLine2 = addNewLawyerLine2.substring(0, addNewLawyerLine2.length - 1) + ending;
+        addNewLawyerLine2 = addNewLawyerLine2.substring(0, addNewLawyerLine2.length - 2) + ending;
 
 
         console.log('SQL: ' + addNewLawyerLine1 + addNewLawyerLine2);
@@ -169,55 +188,55 @@ exports.create = function (app, connection) {
             } else {
                 res.json({ success: true, results: results });
             }
-            
+
 
         });
     });
     var getLawyerById = function (req, res) {
         var userId = req.query.id;
-        connection.query('SELECT * FROM lawyers LEFT JOIN lawyer_language ON lawyers.id = lawyer_language.lawyerID LEFT JOIN lawyer_service ON lawyers.id = lawyer_service.lawyerID WHERE lawyers.id = '+ userId, function (err, results) {
-                if (err)
-                    throw err;
-                    var languages = [];
-                languages[0] = results[0].languageID;
-                var services = [];
-                services[0] = results[0].serviceID;
-                var isPresent;
-                for (var i = 1; i < results.length; i++){
-                    isPresent = true;
-                    for (var a = 0; a < languages.length; a++){
-                    if(results[i].languageID == languages[a]){
+        connection.query('SELECT * FROM lawyers LEFT JOIN lawyer_language ON lawyers.id = lawyer_language.lawyerID LEFT JOIN lawyer_service ON lawyers.id = lawyer_service.lawyerID WHERE lawyers.id = ' + userId, function (err, results) {
+            if (err)
+                throw err;
+            var languages = [];
+            languages[0] = results[0].languageID;
+            var services = [];
+            services[0] = results[0].serviceID;
+            var isPresent;
+            for (var i = 1; i < results.length; i++) {
+                isPresent = true;
+                for (var a = 0; a < languages.length; a++) {
+                    if (results[i].languageID == languages[a]) {
                         isPresent = false;
                         console.log("here " + results[i].languageID);
                     }
-                    
-                    
-                    }
-                    if(isPresent){
-                    languages.push(results[i].languageID);
-                    }
+
+
                 }
-                for (var i = 1; i < results.length; i++){
-                    isPresent = true;
-                    for (var a = 0; a < services.length; a++){
-                    if(results[i].serviceID == services[a]){
+                if (isPresent) {
+                    languages.push(results[i].languageID);
+                }
+            }
+            for (var i = 1; i < results.length; i++) {
+                isPresent = true;
+                for (var a = 0; a < services.length; a++) {
+                    if (results[i].serviceID == services[a]) {
                         isPresent = false;
                         console.log("here " + results[i].serviceID);
                     }
-                    
-                    
-                    }
-                    if(isPresent){
-                    services.push(results[i].serviceID);
-                    }
+
+
                 }
-                console.log("services" + services + " languages:" + languages);
-                
-                    var lawyer = results[0];
-                    lawyer.languages = languages;
-                    lawyer.services = services;
-                    res.json({ data: lawyer, success: true});
-                
+                if (isPresent) {
+                    services.push(results[i].serviceID);
+                }
+            }
+            console.log("services" + services + " languages:" + languages);
+
+            var lawyer = results[0];
+            lawyer.languages = languages;
+            lawyer.services = services;
+            res.json({ data: lawyer, success: true });
+
         });
 
     };
@@ -228,13 +247,13 @@ exports.create = function (app, connection) {
             return;
         }
         connection.query('DELETE FROM lawyers WHERE id=' + userId, function (delErr) {
-                if (delErr)
-                    throw delErr;
-                connection.query('SELECT id, email, name FROM lawyers', function (selectErr, results) {
-                    if (selectErr)
-                        throw selectErr;
-                    res.json({ data: results, success: true});
-                });
+            if (delErr)
+                throw delErr;
+            connection.query('SELECT id, email, name FROM lawyers', function (selectErr, results) {
+                if (selectErr)
+                    throw selectErr;
+                res.json({ data: results, success: true });
+            });
         });
 
     });
