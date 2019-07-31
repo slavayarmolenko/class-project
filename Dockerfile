ae0b6ddcaa5db19cdf89cddfdf77fe57027dad06
Slava <slava.yarmolenko@gmail.com>
2019-07-30 19:58:23 -0700

    Added mysql container

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,10 @@ FROM node:8.1.0-alpine
 WORKDIR /app
 # Install app dependencies
 COPY package.json .
-RUN npm install
-
 ADD . .
+RUN npm install
 ENV FLASK_APP App.jsx
 ENV FLASK_RUN_HOST 0.0.0.0
 
 
-CMD [ "npm", "start" ]
+CMD [ "npm", "run", "dev" ]
--- api/service.js
+++ api/service.js
@@ -9,6 +9,7 @@ var utilsService = require('./utils');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var dbconfig = require("/etc/dbconfig.json")
 
 app.use(session({ secret: 'this-is-a-secret-token', cookie: { maxAge: 1200000 }}));
 app.all('*', function (req, res, next) {
@@ -18,10 +19,10 @@ app.all('*', function (req, res, next) {
 });
 var pool  = mysql.createPool({
     connectionLimit : 10,
-    host: "classdb.c1fc1qmtlpg9.us-west-1.rds.amazonaws.com",
-    user: "master",
-    password: "dEbi07oOFHaAW1s",
-    database: "test1"
+    host: dbconfig.address,
+    user: dbconfig.login,
+    password: dbconfig.pass,
+    database: dbconfig.dbname
   });
  /*var connection = mysql.createConnection({
     host: "classdb.c1fc1qmtlpg9.us-west-1.rds.amazonaws.com",
